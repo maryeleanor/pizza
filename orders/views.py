bbeca4b1f71a8be199b676b1af24e73289fc4e0d
maryeleanor <marycasserly@gmail.com>
2020-06-11 18:10:47 -0700

    started with basic html and css for order page

--- orders/views.py
+++ orders/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    return render(request, "orders/index.html")
+
+
+def menu(request):
+    menu = "Here's the  menu from  the db"
+    return render(request, "orders/menu.html",  {
+        "menu": menu
+    })
